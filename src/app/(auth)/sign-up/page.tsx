265450ed0be5056c1a55139a3edbd91bc5010777
melinamontelongo <110259601+melinamontelongo@users.noreply.github.com>
2023-07-08 00:33:54 -0300

    feat: auth pages not available to logged in users

--- src/app/(auth)/sign-up/page.tsx
+++ src/app/(auth)/sign-up/page.tsx
@@ -1,16 +1,21 @@
 import SignUp from "@/components/auth/SignUp";
+import { getAuthSession } from "@/lib/auth";
 import Link from "next/link";
 import { FiChevronLeft } from "react-icons/fi";
+import { redirect } from "next/navigation";
 
-const SignUpPage = () => {
+const SignUpPage = async () => {
+    const session = await getAuthSession();
+    //  Logged in users cannot access sign up page
+    if (session) return redirect("/");
     return (<>
         <div className="h-screen max-w-2xl mx-auto flex flex-col items-center justify-center gap-5">
-          <Link href="/" className="self-start mt-5">
+            <Link href="/" className="self-start mt-5">
                 <div className="flex items-center">
                     <FiChevronLeft />
                     <span>Go back</span>
                 </div>
-            </Link> 
+            </Link>
             <SignUp />
         </div>
     </>
--- src/components/auth/SignUp.tsx
+++ src/components/auth/SignUp.tsx
@@ -1,12 +1,7 @@
 import Link from "next/link";
 import SignUpForm from "./SignUpForm";
-import { redirect } from "next/navigation";
-import { getAuthSession } from "@/lib/auth";
 
 const SignUp = async() => {
-    const session = await getAuthSession();
-        //  Logged in users cannot access sign up page
-    if (session) return redirect("/");
     return (
         <div className="space-y-5 flex flex-col justify-items-center">
             <div>
