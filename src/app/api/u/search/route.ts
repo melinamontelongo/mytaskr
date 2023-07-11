c68ce99932f04da46c5cc736ba48a9063069e20d
melinamontelongo <110259601+melinamontelongo@users.noreply.github.com>
2023-07-11 13:07:15 -0300

    fix: user cannot search themselves

--- src/app/api/u/search/route.ts
+++ src/app/api/u/search/route.ts
@@ -1,16 +1,22 @@
+import { getAuthSession } from "@/lib/auth";
 import { db } from "@/lib/db";
 
 export async function GET(req: Request){
     const url = new URL(req.url);
     const userQuery = url.searchParams.get("u");
+    const session = await getAuthSession();
 
     if(!userQuery) return new Response("Invalid query", {status: 400});
-
+    if(!session?.user) return new Response("Unauthorized", {status: 401});
+    
     const foundUsers = await db.user.findMany({
         where: {
             email: {
                 startsWith: userQuery,
-            }
+            },
+            NOT: {
+                id: session.user.id,
+              },
         },
         select:{
             id: true,
