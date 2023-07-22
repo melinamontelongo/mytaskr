000bfbb5059d7fb24cf12c06d8a7f974141c39c9
melinamontelongo <110259601+melinamontelongo@users.noreply.github.com>
2023-07-22 13:26:41 -0300

    fix: show boards created by user

--- src/components/Dashboard.tsx
+++ src/components/Dashboard.tsx
@@ -1,3 +1,4 @@
+import React from "react";
 import { getAuthSession } from "@/lib/auth";
 import { db } from "@/lib/db";
 import Link from "next/link";
@@ -42,7 +43,7 @@ const Dashboard = async () => {
     return (
         <div className="h-full max-w-2xl mx-auto flex flex-col gap-5 md:pt-30 pt-36 box-content">
             <h1 className="text-2xl font-bold">Welcome, {user.name ? user.name : user.email}.</h1>
-                <div className="divider"></div>
+            <div className="divider"></div>
             <div>
                 <h3 className="text-2xl font-bold">Your workspaces</h3>
                 <div className="flex md:flex-row flex-col md:flex-wrap gap-4 my-4">
@@ -67,20 +68,32 @@ const Dashboard = async () => {
             <div>
                 <h3 className="text-2xl font-bold">Your boards</h3>
                 <div className="flex md:flex-row flex-col flex-wrap gap-4 my-4">
-                    {user.workspaces && user.workspaces.map((w) => {
-                        return (
-                            <>
-                                {w.boards ? w.boards.map((board) => {
-                                    return (
-                                        <DisplayCard key={board.id} linkHref={`/b/${board.id}`} title={board.name} text={board.description} />
-                                    )
-                                })
-                                    :
-                                    <p>You have no boards. <Link href="board/create" className="underline">Create one.</Link></p>
-                                }
-                            </>
-                        )
-                    })
+                    {user.workspaces || user.createdWorkspaces ? (<>
+                        {user.workspaces?.map((w) => {
+                            return (
+                                <React.Fragment key={`workspaces${w.id}`}>
+                                    {w.boards.map((board) => {
+                                        return (
+                                            <DisplayCard key={board.id} linkHref={`/b/${board.id}`} title={board.name} text={board.description} />
+                                        )
+                                    })}
+                                </React.Fragment>
+                            )
+                        })}
+                        {user.createdWorkspaces?.map((w) => {
+                            return (
+                                <React.Fragment key={`createdWorkspaces${w.id}`}>
+                                    {w.boards.map((board) => {
+                                        return (
+                                            <DisplayCard key={board.id} linkHref={`/b/${board.id}`} title={board.name} text={board.description} />
+                                        )
+                                    })}
+                                </React.Fragment>
+                            )
+                        })}
+                    </>)
+                        :
+                        <p>You have no boards. <Link href="board/create" className="underline">Create one.</Link></p>
                     }
                 </div>
             </div>
