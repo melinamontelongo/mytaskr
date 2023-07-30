7dbc95c400eeb0c31979e605ba212883afcd374a
melinamontelongo <110259601+melinamontelongo@users.noreply.github.com>
2023-07-29 21:25:36 -0300

    feat: middleware to redirect users who are not logged in

---
+++ src/middleware.ts
@@ -0,0 +1,14 @@
+import { getToken } from "next-auth/jwt";
+import { NextRequest, NextResponse } from "next/server";
+
+//  User must be logged in to view certain pages
+export async function middleware(req: NextRequest) {
+    const token = await getToken({ req });
+    if (!token) {
+        return NextResponse.redirect(new URL("/sign-in", req.nextUrl));
+    }
+}
+
+export const config = {
+    matcher: ["/u/profile", "/b/create", "/w/create"],
+}
