a2dbb3c2ec7fc744650e4842c325fee8ca984e27
melinamontelongo <110259601+melinamontelongo@users.noreply.github.com>
2023-08-01 13:31:51 -0300

    style: modal body margin and close icon

--- src/components/ui/Modal.tsx
+++ src/components/ui/Modal.tsx
@@ -1,5 +1,6 @@
 "use client"
 import { ForwardedRef, ReactNode, forwardRef } from "react";
+import { AiOutlineClose } from "react-icons/ai";
 
 interface ModalProps {
     id: string,
@@ -13,12 +14,14 @@ const Modal = forwardRef(({ id, body, actionContent }: ModalProps, ref: Forwarde
             <input ref={ref} type="checkbox" id={id} className="modal-toggle" />
             <div className="modal">
                 <div className="modal-box bg-base-200">
-                    <label htmlFor={id} aria-label="close" className="btn btn-sm btn-circle btn-ghost absolute right-2 top-2">X</label>
-                    {body}
-                    {actionContent &&
-                        <div className="modal-action">
-                            {actionContent}
-                        </div>}
+                    <label htmlFor={id} aria-label="close" className="btn btn-sm btn-circle btn-ghost absolute right-2 top-2"><AiOutlineClose /></label>
+                    <div className="mt-5">
+                        {body}
+                        {actionContent &&
+                            <div className="modal-action">
+                                {actionContent}
+                            </div>}
+                    </div>
                 </div>
             </div>
         </>
