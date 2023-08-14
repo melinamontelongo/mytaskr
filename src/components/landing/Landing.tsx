097fb5d8860cc9a894ae249871d82cff4fa3075e
melinamontelongo <110259601+melinamontelongo@users.noreply.github.com>
2023-08-14 15:04:11 -0300

    style: landing page alignment

--- src/components/landing/Landing.tsx
+++ src/components/landing/Landing.tsx
@@ -4,18 +4,18 @@ import Footer from "../ui/Footer";
 
 const Landing = () => {
     return (<>
-            <div className="pt-24 flex flex-col gap-10 ">
-                <div className="flex md:flex-row flex-col items-center justify-center sm:justify-between px-5 gap-5">
+            <div className="pt-24 pb-10 flex flex-col px-5 gap-10 min-h-screen">
+                <div className="flex md:flex-row flex-col items-center justify-center sm:justify-around px-5 gap-5">
                     <div className="space-y-5">
                         <h2 className="font-extrabold text-5xl drop-shadow-md">Your tasks made easier</h2>
                         <p className="text-xl">Organize everything from daily chores to big projects - <span className="font-bold">for free.</span></p>
-                        <Link className="btn btn-primary normal-case rounded" href="/sign-up">Register now!</Link>
+                        <Link className="btn btn-primary normal-case rounded w-full md:w-auto" href="/sign-up">Register now!</Link>
                     </div>
                     <div>
                         <Image src="/landing1.png" height={600} width={600} alt="landing image" quality={100} />
                     </div>
                 </div>
-                <div className="flex md:flex-row flex-col items-center justify-center sm:justify-between px-5 gap-5">
+                <div className="flex md:flex-row flex-col items-center justify-center sm:justify-around px-5 gap-5">
                     <div className="order-last md:order-first">
                         <Image src="/landing2.png" height={600} width={600} alt="landing image" quality={100} />
                     </div>
@@ -24,7 +24,7 @@ const Landing = () => {
                         <p className="text-xl">Achieve your objectives, work with your team, meet those deadlines.</p>
                     </div>
                 </div>
-                <div className="flex md:flex-row flex-col items-center justify-center sm:justify-between px-5 gap-5">
+                <div className="flex md:flex-row flex-col items-center justify-center sm:justify-around px-5 gap-5">
                     <div className="space-y-5">
                         <h3 className="font-extrabold text-4xl drop-shadow-md">Overview</h3>
                         <p className="text-xl">Keep everything in one place with <span className="font-bold">workspaces</span> - you can create as many as you need!</p>
@@ -36,14 +36,14 @@ const Landing = () => {
                         <Image src="/overview.png" height={900} width={900} alt="landing image overview" quality={100} />
                     </div>
                 </div>
-                <div className="flex md:flex-row flex-col items-center justify-center sm:justify-between px-5 gap-5">
+                <div className="flex md:flex-row flex-col items-center justify-center sm:justify-around px-5 gap-5">
                     <div className="order-last md:order-first">
                         <Image src="/landing3.png" height={600} width={600} alt="landing image overview" quality={100} />
                     </div>
                     <div className="space-y-5">
                         <h3 className="font-extrabold text-4xl drop-shadow-md">Get started</h3>
                         <p className="text-xl">Join today and enjoy all of <span className="font-bold">mytaskr</span> features for free.</p>
-                        <Link className="btn btn-primary normal-case rounded" href="/sign-up">Go!</Link>
+                        <Link className="btn btn-primary normal-case rounded w-full md:w-auto" href="/sign-up">Go!</Link>
                     </div>
                 </div>
             </div>
