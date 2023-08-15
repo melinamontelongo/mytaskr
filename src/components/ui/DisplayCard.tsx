a2e52c0d4cb96dd0081d0b98598d5fae3f7204ac
melinamontelongo <110259601+melinamontelongo@users.noreply.github.com>
2023-08-14 21:22:22 -0300

    style: display card

--- src/components/ui/DisplayCard.tsx
+++ src/components/ui/DisplayCard.tsx
@@ -13,7 +13,7 @@ const DisplayCard = ({ title, text, linkHref, backgroundImageSrc }: DisplayCardP
         <Link href={linkHref} className="mx-auto md:mx-0">
             <div className={`card w-40 sm:w-60 md:w-80 h-32 bg-base-200 shadow-md hover:shadow-xl hover:scale-105 transition-transform ${backgroundImageSrc && "image-full"}`}>
                 {backgroundImageSrc && <figure><Image src={backgroundImageSrc} alt={`${title} board background image`} style={{ objectFit: "cover" }} className="rounded" fill={true} /></figure>}
-                <div className="card-body break-all md:break-words w-40 sm:w-62 md:w-80 ">
+                <div className="card-body break-words w-40 sm:w-60 md:w-80 h-32">
                     <h2 className="card-title">{title}</h2>
                     {text &&
                         <p className="truncate">{text}</p>
